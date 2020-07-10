9d68f9c1fb405a049144984c5681a9122d274b10
Miriam Ruiz <miriam@debian.org>
2020-07-10 23:09:06 +0200

    Improve music staff

--- components/music_staff.py
+++ components/music_staff.py
@@ -20,12 +20,21 @@ from .musical_info import MusicDefs
 
 class MusicStaffElement(layout.root.LayoutElement):
 
+    SCORE_MIN_NOTE, SCORE_MAX_NOTE = 36, 84 # from C2 to C6, both included
+    SCORE_LINES = (43, 47, 50, 53, 57, 64, 67, 71, 74, 77) # G2, B2, D3, F3, A3, E4, G4, B4, D5, F5
+    DIATONIC_NOTES = { 0: 0, 1: 0, 2: 1, 3: 1, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 9: 5, 10: 5, 11: 6 }
+
     def __init__(self, music_info):
         self.music_info = music_info
         self.border_gap = 10.
         width, height = 300, 7 * 4 * 5
-        self.ims = self.get_base_image(width, height)
         self.size = layout.datatypes.Point(width + self.border_gap * 2, height + self.border_gap * 2)
+
+        self.clefs_svg = Rsvg.Handle().new_from_file(os.path.join("artwork", "clefs.svg"))
+        #~ self.ims = self.get_base_image(width, height)
+
+    def __del__(self):
+        self.clefs_svg.close()
 
     def get_minimum_size(self, ctx):
         return self.size
@@ -38,12 +47,11 @@ class MusicStaffElement(layout.root.LayoutElement):
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
         ctx = cairo.Context(surface)
 
-        rsvg_handle = Rsvg.Handle()
-        svg = rsvg_handle.new_from_file(os.path.join("artwork", "grand_staff.svg"))
-        dimensions = svg.get_dimensions()
         vp = Rsvg.Rectangle()
         vp.x, vp.y, vp.width, vp.height = 0, 0, width, height
-        svg.render_document(ctx, vp)
+        #~ self.clefs_svg.render_document(ctx, vp)
+        self.clefs_svg.render_element(ctx, "#treble", vp)
+        self.clefs_svg.render_element(ctx, "#bass", vp)
 
         ctx.set_source_rgba(1, 0, 0, 1);
         ctx.set_line_width(5);
@@ -51,9 +59,15 @@ class MusicStaffElement(layout.root.LayoutElement):
         ctx.line_to(200, 200);
         ctx.stroke();
 
-        svg.close()
-
         return surface
+
+    def yslot_for_note(self, note):
+            octave = note // 12
+            cromatic_note = note % 12
+            diatonic_note = self.DIATONIC_NOTES[cromatic_note]
+            yslot = ( (7 - octave) * 7 - diatonic_note )
+            print(f"Score Note {note}: octave = {octave} + cromatic = {cromatic_note} -> diatonic = {diatonic_note} -> yslot = {yslot}")
+            return yslot
 
     def render(self, rect, ctx):
         xpos, ypos, width, height = rect.get_data()
@@ -63,31 +77,46 @@ class MusicStaffElement(layout.root.LayoutElement):
 
         ctx.save()
 
-        ctx.set_operator(cairo.OPERATOR_OVER);
-        ctx.set_source_surface(self.ims, xpos + self.border_gap, ypos + self.border_gap)
-        ctx.paint()
+        #~ ctx.set_operator(cairo.OPERATOR_OVER);
+        #~ ctx.set_source_surface(self.ims, xpos + self.border_gap, ypos + self.border_gap)
+        #~ ctx.paint()
 
-        score_lines = (43, 47, 50, 53, 57, # G2, B2, D3, F3, A3
-                       64, 67, 71, 74, 77) # E4, G4, B4, D5, F5
-        diatonic_notes = { 0: 0, 1: 0, 2: 1, 3: 1, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 9: 5, 10: 5, 11: 6 }
-        for note in range (36, 84 + 1): # from C2 to C6, both included
-            octave = note // 12
-            cromatic_note = note % 12
-            diatonic_note = diatonic_notes[cromatic_note]
+        score_xpos = xpos + self.border_gap
+        score_ypos = ypos + self.border_gap
+        score_width = (width - 2 * self.border_gap)
+        score_height = (height - 2 * self.border_gap)
+        yslot_height = score_height / (4 * 7)
 
-            y = ypos + self.border_gap + (height - 2 * self.border_gap) * ( (7 - octave) * 7 - diatonic_note ) / (4 * 8)
-
-            ctx.set_source_rgb(0., 0., 0.)
-            ctx.set_line_width(1)
-            if note in score_lines:
-                ctx.move_to(cx + note - 150, y)
-                ctx.line_to(cx + note + 150, y)
-            else:
-                ctx.move_to(cx + note - 20, y)
-                ctx.line_to(cx + note + 20, y)
+        ctx.set_source_rgb(0.0, 0.1, 0.3)
+        ctx.set_line_width(1)
+        for note in self.SCORE_LINES:
+            yslot = self.yslot_for_note(note)
+            y = score_ypos + yslot * yslot_height
+            ctx.move_to(score_xpos, y)
+            ctx.line_to(score_xpos + score_width, y)
             ctx.stroke()
 
-            ctx.arc(xpos + 50 + 12 * note, y, 4, 0, 2. * math.pi)
+        vp = Rsvg.Rectangle()
+        vp.x, vp.y, vp.width, vp.height = score_xpos, score_ypos + self.yslot_for_note(80) * yslot_height, score_width, yslot_height * 11
+        self.clefs_svg.render_element(ctx, "#treble", vp)
+        vp.x, vp.y, vp.width, vp.height = score_xpos, score_ypos + self.yslot_for_note(61) * yslot_height, score_width, yslot_height * 11
+        self.clefs_svg.render_element(ctx, "#bass", vp)
+
+        for note in range(self.SCORE_MIN_NOTE, self.SCORE_MAX_NOTE + 1):
+            x = cx + 10 * (note % 12)
+            yslot = self.yslot_for_note(note)
+            y = score_ypos + yslot * yslot_height
+
+            ledger_line = note not in self.SCORE_LINES
+
+            if ledger_line:
+                ctx.set_source_rgb(0., 0., 0.)
+                ctx.set_line_width(1)
+                ctx.move_to(x - 6, y)
+                ctx.line_to(x + 6, y)
+                ctx.stroke()
+
+            ctx.arc(x, y, 4, 0, 2. * math.pi)
             ctx.fill()
 
         ctx.restore()
